b15992cc59c454f31a50dff1caafbcd100bf0a5e
Raed Naamoun <raed.naamoun@stud.h-da.de>
2026-04-26 16:51:26 +0100

    Tailwindcss config, assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 const page = () => {
   return (
-    <div>
-      <h1>Welcome to the course</h1>
-    </div>
+    <>
+      <h1 className="h1-bold">Welcome to the course</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to the course</h1>
+    </>
   );
 };
 
