69cb6038529d7170eff9b93d6fba948701d6e63e
Raed Naamoun <raed.naamoun@stud.h-da.de>
2026-06-01 16:00:57 +0200

    Implement Sign In Functionality

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,14 +1,18 @@
 "use client";
-import AuthForm from '@/components/Forms/AuthForm'
-import { SignInSchema } from '@/lib/validations'
+
+import AuthForm from "@/components/Forms/AuthForm";
+import { signInWithCredentials } from "@/lib/actions/auth.action";
+import { SignInSchema } from "@/lib/validations";
 
 const SignIn = () => {
   return (
-    <AuthForm formType="SIGN_IN"
-          schema={SignInSchema}
-          defaultValues={{ email: "", password: "" }}
-          onSubmit={(data) => Promise.resolve({ success: true, data })}/>
-  )
-}
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={signInWithCredentials}
+    />
+  );
+};
 
-export default SignIn
+export default SignIn;
